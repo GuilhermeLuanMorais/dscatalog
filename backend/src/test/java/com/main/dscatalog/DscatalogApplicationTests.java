56d935b027b2de74a65613957800492cb3e1aa21
Guilherme <guimorais006@hotmail.com>
2023-04-16 21:35:31 -0300

    Category Resource

---
+++ backend/src/test/java/com/main/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

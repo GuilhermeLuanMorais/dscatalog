4bbd741e696d817cb9e83d1f784ac14578cee4e1
Guilherme <guimorais006@hotmail.com>
2023-10-08 21:30:34 -0300

    # Refactory

--- backend/src/test/java/com/main/simpleitstore/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/main/simpleitstore/repositories/ProductRepositoryTests.java
@@ -3,27 +3,41 @@ package com.main.simpleitstore.repositories;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.main.simpleitstore.entities.Product;
 
 @DataJpaTest
 public class ProductRepositoryTests {
 	
+	Long existingId;
+	long nonExistingId;
+	
+	@BeforeEach
+	void setUp() {
+		existingId = 1L;
+		nonExistingId = 1000L;
+	}
+	
 	@Autowired
 	private ProductRepository productRepository;
 	
 	@Test
 	public void deleteShouldDeleteObjectWhenIdExists() {
-		Long existingId = 1L;
-		
 		productRepository.deleteById(existingId);
-		
 		Optional<Product> result = productRepository.findById(existingId);
-		
 		Assertions.assertFalse(result.isPresent());
+	}
+	
+	@Test
+	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+			productRepository.deleteById(nonExistingId);
+		});
 	}
 
 }
